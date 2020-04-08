e45e4a2b512095497b57e71d66dc7936e20f27b0
Jes Struck <mail@jesstruck.dk>
2020-04-08 15:08:38 +0200

    Installing as CLI app

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="etx-sum", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Jes Struck",
     author_email="mail@jesstruck.dk",
     description="ext-sum.py is intended to be a small utillity script that gives a few greate stats on your daytrading results",
@@ -13,6 +13,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/jesstruck/etx-summarizer",
     packages=setuptools.find_packages(),
+    entry_points = {
+      'console_scripts': [
+        'etx-sum = etx.__main__:main'
+      ],
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
